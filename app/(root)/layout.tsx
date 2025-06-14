0cc3b968bd9c6c427f854b005ece7279fd3be773
shfinaz <shafinaz0208@gmail.com>
2025-06-14 22:11:51 +0800

    Left Side Bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,22 @@
-import Navbar from '@/components/navigation/navbar'
-import React, { ReactNode } from 'react'
+import { ReactNode } from "react";
 
-const Rootlayout = ({children}: {children: ReactNode}) => {
+import Navbar from "@/components/navigation/navbar";
+import LeftSidebar from "@/components/navigation/LeftSidebar";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main>
-        <Navbar />
-        {children}
-    </main>
-  )
-}
+    <main className="background-light850_dark100 realtive">
+      <Navbar />
 
-export default Rootlayout;
+      <div className="flex">
+        <LeftSidebar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-10xl">{children}</div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default RootLayout;
