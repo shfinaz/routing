2fb7a0a5d583e28d388a3c5d556b5e7f10a9f235
shfinaz <shafinaz0208@gmail.com>
2025-06-17 11:47:38 +0800

    Implement Local Search Bar

--- constants/routes.ts
+++ constants/routes.ts
@@ -4,6 +4,7 @@ const ROUTES = {
     SIGN_UP: "/sign-up",
     PROFILE: (id: string) => '/profile/${id}',
     TAGS: (id:string) => '/tags/${id}',
+    ASK_QUESTIONS: "/ask-questions",
 }
 
 export default ROUTES;
