90d986728a1d7dc8b0690976ff797a46dbc1ffa4
shfinaz <shafinaz0208@gmail.com>
2025-06-30 22:23:27 +0800

    Implement Answer Model

---
+++ database/answer.model.ts
@@ -0,0 +1,21 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IAnswer {
+    author: Types.ObjectId; // Reference to User
+    question: Types.ObjectId; // Reference to Question  
+    content: string; // The content of the answer
+    upvotes: number; // Number of upvotes
+    downvotes: number; // Number of downvotes
+}
+
+const AnswerSchema = new Schema<IAnswer>({
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+    content: { type: String, required: true },
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+}, { timestamps: true });
+
+const Answer = models?.Account || model<IAnswer>("Account", AnswerSchema);
+
+export default Answer;
